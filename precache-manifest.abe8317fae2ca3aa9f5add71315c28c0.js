542db07893fe539930b243811cf69414f7df2c2b
ermik <email@ermolay.com>
2019-07-15 01:59:33 -0400

    Updates

---
+++ precache-manifest.abe8317fae2ca3aa9f5add71315c28c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c80cdcd02372a4369b242a0956092b4",
+    "url": "/challenge-2-list-repos/index.html"
+  },
+  {
+    "revision": "fb3fb2a132891075e786",
+    "url": "/challenge-2-list-repos/static/css/main.75cec10b.chunk.css"
+  },
+  {
+    "revision": "d33ec7f1ef14219b6fa2",
+    "url": "/challenge-2-list-repos/static/js/2.5724c2e5.chunk.js"
+  },
+  {
+    "revision": "fb3fb2a132891075e786",
+    "url": "/challenge-2-list-repos/static/js/main.f4e76d9d.chunk.js"
+  },
+  {
+    "revision": "918c93fa6fbbc3b9619a",
+    "url": "/challenge-2-list-repos/static/js/runtime~main.e67b6a80.js"
+  }
+]);
